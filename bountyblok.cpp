568dad1b104abe95d5c47bcb28cb90c9e857828d
bountyblok <53401686+bountyblok@users.noreply.github.com>
2019-11-08 11:52:11 -0500

    Add files via upload

---
+++ bountyblok.cpp
@@ -0,0 +1,36 @@
+#include <eosio/eosio.hpp>
+#include <eosio/asset.hpp>
+
+using namespace eosio;
+
+class [[eosio::contract("bountyblok")]] bountyblok : public contract {
+  public:
+  	using contract::contract;
+
+	[[eosio::action]]
+	void execchlgdone(name receiver, name account_name, std::string challenge_name, std::string challenge_guid, uint64_t create_date, asset reward) {
+		notify_challenge_done_action notichlgdone(get_self(), {get_self(), "active"_n});
+		notichlgdone.send(receiver, account_name, challenge_name, challenge_guid, create_date, reward);
+	}
+
+	[[eosio::action]]
+	void notichlgdone(name receiver, name account_name, std::string challenge_name, std::string challenge_guid, uint64_t create_date, asset reward) {
+		require_auth(get_self());
+		require_recipient(receiver);
+	}
+	
+	[[eosio::action]]
+	void logtask(std::string task_guid, std::string task_data, std::string account_name, std::string create_date) {}
+
+	[[eosio::action]]
+	void addtask(std::string task_guid, std::string task_data, std::string account_name, std::string create_date) {}
+
+	[[eosio::action]]
+	void addchallenge(std::string challenge_guid, std::string challenge_data, std::string account_name, std::string create_date) {}
+
+	[[eosio::action]]
+	void addsocial(std::string from, std::string to, std::string social_data, std::string create_date) {}
+	
+	using notify_challenge_done_action = action_wrapper<"notichlgdone"_n, &bountyblok::notichlgdone>;
+};
+
