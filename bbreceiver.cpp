2cf76098d7e686be8f2ca6fd8f08b70afa26b6c0
bountyblok <53401686+bountyblok@users.noreply.github.com>
2019-11-08 12:13:53 -0500

    Add files via upload

--- bbreceiver.cpp
+++ bbreceiver.cpp
@@ -7,12 +7,17 @@ class [[eosio::contract("bbreceiver")]] bbreceiver : public contract {
   public:
   	 using contract::contract;
 	 
+	/**
+	 *  Implementing the bountyblok::notichlgdone allows this smart contract
+	 *  to obtain a copy of the parameters when a challenge has been completed.
+	 *  Ex: Transfer the reward to the account_name who completed the Challenge.
+	 *  Ex: Simply store it in a Datatable to process later.
+	 */
 	[[eosio::on_notify("bountyblok::notichlgdone")]]
 	void onnotified(name receiver, name account_name, std::string challenge_name, std::string challenge_guid, uint64_t create_date, asset reward) {
-		// handle received notification for completing a challenge:
-		// - transfer reward to account_name
-		// - store in a multi_index table
-		// - etc.
+		
+		// handle received notification for completing a challenge
+
 	}
 
 	[[eosio::action]]
